5441f850834c8222313c75dd1e54a4936aa26288
Jack Franklin <jack@jackfranklin.net>
2013-08-06 20:20:43 +0100

    add menu icon

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
-$(function() {
-  $("code").attr("data-language", "javascript");
+$('body').addClass('js');
+$('.nav-toggle').on('click', function(e) {
+  e.preventDefault();
+  $('.nav-primary').find('ul').toggleClass('open');
+});
 
-  Rainbow.color();
- });
+var menu = $(".menu-icon");
+
+menu.on("click", function(e) {
+  e.preventDefault();
+  var header = $(".site-header");
+  if(header.css("margin-left") != "0px") {
+    header.add(".main-wrapper").animate({
+      "margin-left": 0
+    }, 200);
+  } else {
+    header.add(".main-wrapper").animate({
+      "margin-left": "13.125em"
+    }, 200);
+  }
+
+});
