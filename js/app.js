91db0193eb5b4230f3da575c2fb5ff826d563e32
Jack Franklin <jack@jackfranklin.net>
2013-08-10 16:39:59 +0100

    add archives search

--- js/app.js
+++ js/app.js
@@ -18,5 +18,21 @@ menu.on("click", function(e) {
       "margin-left": "13.125em"
     }, 200);
   }
+});
 
+var searchBox = $("input[name='searchterm']");
+var allPosts = $(".archives h4 a");
+console.log(allPosts);
+
+searchBox.on("keyup", function(e) {
+  var searchValue = this.value.toLowerCase();
+  console.log(searchValue);
+  allPosts.each(function() {
+    var self = $(this);
+    if(self.text().toLowerCase().indexOf(searchValue) > -1) {
+      self.parents("li").show();
+    } else {
+      self.parents("li").hide();
+    }
+  });
 });
