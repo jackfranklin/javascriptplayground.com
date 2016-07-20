ed8a29e8413391f91970322eec1a9e214b95551c
Jack Franklin <jack@jackfranklin.net>
2016-07-20 19:23:29 +0200

    Tidy up the JS

--- js/app.js
+++ js/app.js
@@ -1,22 +1,35 @@
-$('body').addClass('js');
-$('.nav-toggle').on('click', function(e) {
+document.body.classList.add('js');
+
+var navToggle = document.querySelector('.nav-toggle');
+
+navToggle.addEventListener('click', function(e) {
   e.preventDefault();
-  $('.nav-primary').find('ul').toggleClass('open');
+  document.querySelector('.nav-primary ul').classList.toggle('open');
 });
 
+var searchForm = document.querySelector('.search-form');
 
-var searchBox = $("input[name='searchterm']");
-var allPosts = $(".archives h4 a");
+// code below is for the archives page only
+// so we won't execute it if we can't find the search form
+if (searchForm) {
 
-searchBox.on("keyup", function(e) {
-  var searchValue = this.value.toLowerCase();
-  console.log(searchValue);
-  allPosts.each(function() {
-    var self = $(this);
-    if(self.text().toLowerCase().indexOf(searchValue) > -1) {
-      self.parents("li").show();
-    } else {
-      self.parents("li").hide();
-    }
+  searchForm.addEventListener('submit', function(e) {
+    // we don't want the form to actually submit
+    e.preventDefault();
   });
-});
+
+  var searchBox = document.querySelector('.search-form input');
+
+  var allPosts = document.querySelectorAll('.archives h4 a');
+
+  searchBox.addEventListener('keyup', function(e) {
+    var searchValue = searchBox.value.toLowerCase();
+    Array.prototype.forEach.call(allPosts, function(post) {
+      if (post.text.toLowerCase().indexOf(searchValue) > -1) {
+        post.parentElement.parentElement.style.display = 'block';
+      } else {
+        post.parentElement.parentElement.style.display = 'none';
+      }
+    });
+  });
+}
