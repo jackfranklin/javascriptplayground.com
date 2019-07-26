c22419b54517b5ec0a2e334eeede28f5a5c6e97f
Jack Franklin <jack@jackfranklin.net>
2019-07-26 11:01:07 +0100

    template some of the rollup stuff

--- code-for-posts/zip-lists/rollup.config.js
+++ code-for-posts/zip-lists/rollup.config.js
@@ -1,26 +1,3 @@
-import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import replace from 'rollup-plugin-replace'
+const template = require('../rollup-template')
 
-export default {
-  input: 'index.jsx',
-  output: {
-    file: 'bundle.js',
-    format: 'iife',
-  },
-  plugins: [
-    resolve({
-      extensions: ['.mjs', '.js', '.jsx', '.json'],
-    }),
-    commonjs(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
-    babel({
-      babelrc: false,
-      presets: [['@babel/env', { modules: false }], '@babel/preset-react'],
-      exclude: 'node_modules/**',
-    }),
-  ],
-}
+module.exports = template
